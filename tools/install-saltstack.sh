8cb2b181929fe2747a2783c755b1a93908079e0c
Alan Ross <aross079@uottawa.ca>
2018-01-17 13:24:06 -0500

    Disabled checking salt-services after installation. This will help install Salt-Stack onto a docker container for testing.

--- tools/install-saltstack.sh
+++ tools/install-saltstack.sh
@@ -2,10 +2,10 @@
 
 bootstrap_master() {
   curl -L https://bootstrap.saltstack.com -o /tmp/install_salt.sh
-  sh /tmp/install_salt.sh -P -M
+  sh /tmp/install_salt.sh -P -M -d
 }
 
 bootstrap_minion() {
   curl -L https://bootstrap.saltstack.com -o /tmp/install_salt.sh
-  sh /tmp/install_salt.sh -P
+  sh /tmp/install_salt.sh -P -d
 }
